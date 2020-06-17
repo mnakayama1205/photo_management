6f80259372ebd137b56d1adf7edbe00a18430e82
Makoto Nakayama <m-nakayama@timedia.co.jp>
2020-06-17 14:34:24 +0900

    add login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'top#show'
+
+  resource :top, only: [:show] do
+    member do
+      post 'login'
+    end
+  end
 end
