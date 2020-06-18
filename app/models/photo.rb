676ebb1fef7769c30d1f3e08ce0f86306777d88a
Makoto Nakayama <m-nakayama@timedia.co.jp>
2020-06-18 22:48:06 +0900

    validates characters limits

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   has_one_attached :photo
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 30 }
   validates :photo, presence: true
 end
